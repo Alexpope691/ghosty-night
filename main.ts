5c012c6a68ad79044d8408eff9b0a3bf660b3e74
codingcodey <76607086+codingcodey@users.noreply.github.com>
2020-12-26 19:29:13 +1100

    Add lives and sound.

--- main.ts
+++ main.ts
@@ -1,171 +1,240 @@
 enum ActionKind {
     Walking,
     Idle,
-    Jumping
+    Jumping,
+    Invincible
 }
 namespace SpriteKind {
     export const Gap = SpriteKind.create()
+    export const DamagedPlayer = SpriteKind.create()
 }
 controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.vy = -100
-    animation.setAction(mySprite, ActionKind.Jumping)
-    mySprite.startEffect(effects.fire, 300)
+    if (mySprite.kind() == SpriteKind.Player) {
+        music.pewPew.play()
+        animation.setAction(mySprite, ActionKind.Jumping)
+        mySprite.startEffect(effects.fire, 300)
+    }
+})
+sprites.onDestroyed(SpriteKind.DamagedPlayer, function (sprite) {
+    createPlayer()
+    mySprite.setPosition(sprite.x, sprite.y)
 })
 function takeDamage () {
-	
+    if (mySprite.kind() == SpriteKind.Player) {
+        music.wawawawaa.play()
+        info.changeLifeBy(-1)
+        mySprite.lifespan = 3000
+        mySprite.setKind(SpriteKind.DamagedPlayer)
+        animation.setAction(mySprite, ActionKind.Invincible)
+    }
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Gap, function (sprite, otherSprite) {
-    if (otherSprite.right - sprite.left < 1) {
+    if (otherSprite.right - sprite.left < 1.1) {
         info.changeScoreBy(1)
+        music.baDing.play()
+        if (info.score() % 10 == 0) {
+            info.changeLifeBy(1)
+            music.powerUp.play()
+        }
     }
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
-    game.over(false)
+    takeDamage()
 })
+function createPlayer () {
+    mySprite = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f 1 1 1 1 f f . . . . 
+        . . . f b 1 1 1 1 1 1 b f . . . 
+        . . . f d 1 1 1 1 1 1 1 f . . . 
+        . . f d d 1 1 1 1 1 1 1 d f . . 
+        . . f d d d 1 1 1 1 1 1 d f . . 
+        . . f d d d d d d 1 1 1 d f . . 
+        . . f b d d d d b f d 1 d f . . 
+        . . f c b b b d c f d d b f . . 
+        . . . f c b b 1 1 1 1 1 f . . . 
+        . . . . f f f f f 1 b 1 f . . . 
+        . . . . f b 1 1 1 c f b f . . . 
+        . . . . f f b 1 b 1 f f . . . . 
+        . . f . f f f b f b f . . . . . 
+        . . f f f f f f f f . . . . . . 
+        . . . f f f f f . . . . . . . . 
+        `, SpriteKind.Player)
+    mySprite.ay = 300
+    anim = animation.createAnimation(ActionKind.Jumping, 25)
+    anim.addAnimationFrame(img`
+        . . . . . . f f f f f . . . . . 
+        . . . . f f b 1 1 1 1 f f . . . 
+        . . . f b 1 1 1 1 1 1 1 b f . . 
+        . . . f 1 1 1 1 1 1 1 1 1 f . . 
+        . . f f f f 1 1 1 1 1 1 1 d f . 
+        . f b 1 1 1 c 1 d d 1 1 1 d f . 
+        . f f b 1 b 1 f d c f 1 1 b f . 
+        . . f . b f b f b 1 1 1 1 1 f . 
+        . . . f f f f c f d b 1 b 1 f . 
+        . . . . f c c c f c f b f b f . 
+        . . . . . f f f f f f f f f f . 
+        . . . . . . f f f f f f . . . . 
+        . . . . . . f f f f f f . . . . 
+        . . f . . f f f f f f f . . . . 
+        . . f f f f f f f f f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        `)
+    anim.addAnimationFrame(img`
+        . . . . . . f f f f f . . . . . 
+        . . . . f f b 1 1 1 1 f f . . . 
+        . . . f b 1 1 1 1 1 1 1 b f . . 
+        . . . f 1 1 1 1 1 1 1 1 1 f . . 
+        . . f f f f 1 1 1 1 1 1 1 d f . 
+        . f b 1 1 1 c 1 d d 1 1 1 d f . 
+        . f f b 1 b 1 f d c f 1 1 b f . 
+        . . f . b f b f b 1 1 1 1 1 f . 
+        . . . f f f f c f d b 1 b 1 f . 
+        . . . . f c c c f c f b f b f . 
+        . . . . . f f f f f f f f f f . 
+        . . . . . . f f f f f f . . . . 
+        . . . . . . f f f f f f . . . . 
+        . . f . . f f f f f f f . . . . 
+        . . f f f f f f f f f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        `)
+    anim.addAnimationFrame(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f 1 1 1 1 f f . . . . 
+        . . . f b 1 1 1 1 1 1 b f . . . 
+        . . . f d 1 1 1 1 1 1 1 f . . . 
+        . . f d d 1 1 1 1 1 1 1 d f . . 
+        . . f d d d 1 1 1 1 1 1 d f . . 
+        . . f d d d d d d 1 1 1 d f . . 
+        . . f b d d d d b f d 1 d f . . 
+        . . f c b b b d c f d d b f . . 
+        . . . f c b b 1 1 1 1 1 f . . . 
+        . . . . f f f f f 1 b 1 f . . . 
+        . . . . f b 1 1 1 c f b f . . . 
+        . . . . f f b 1 b 1 f f . . . . 
+        . . f . f f f b f b f . . . . . 
+        . . f f f f f f f f . . . . . . 
+        . . . f f f f f . . . . . . . . 
+        `)
+    anim.addAnimationFrame(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f 1 1 1 1 f f . . . . 
+        . . . f b 1 1 1 1 1 1 b f . . . 
+        . . f b d 1 1 1 1 1 1 1 f . . . 
+        . . f d d 1 1 1 1 1 1 1 d f . . 
+        . . f d d d 1 1 1 1 1 1 d f . . 
+        . . f d d d d d d 1 1 1 d f . . 
+        . . f d d d d d d 1 1 1 d f . . 
+        . . f b d d d d d d d 1 d f . . 
+        . . f f b b d d b f d 1 d f . . 
+        . . . f c b b d c f d d b f . . 
+        . . . f f f b d d c c f f f f . 
+        . . . f f f f c f b b b 1 b c f 
+        . . f f f f f f f f c d 1 b 1 f 
+        f f f f f f f f f . . f d f d f 
+        . f f f f f f . . . . . f . f . 
+        `)
+    anim.addAnimationFrame(img`
+        . . . . . . f f f f f . . . . . 
+        . . . . f f 1 1 1 1 1 f . . . . 
+        . . . f b 1 1 1 1 1 1 b f . . . 
+        . . f b d 1 1 1 1 1 1 1 f . . . 
+        . . f d d d 1 1 1 1 1 1 d f . . 
+        . . f d d d d 1 1 1 1 1 d f . . 
+        . . f d d d d d d d 1 1 d f . . 
+        . . f d d d d d d d 1 1 1 f . . 
+        . . f d d d d d d c f 1 1 f . . 
+        . . . f b d d d b 1 1 1 1 b f . 
+        . . . . f f f c f d b 1 b 1 f . 
+        . . . f f f f f f f f b f b f . 
+        f f . f f f f f f f f f f f . . 
+        . f f f f f f f f . . . . . . . 
+        . f f f f f f b 1 b 1 f . . . . 
+        . . f f f f f f b f b f . . . . 
+        `)
+    anim.addAnimationFrame(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f 1 1 1 1 f f . . . . 
+        . . . f b 1 1 1 1 1 1 b f . . . 
+        . . . f d 1 1 1 1 1 1 1 f . . . 
+        . . f d d 1 1 1 1 1 1 1 d f . . 
+        . . f d d d 1 1 1 1 1 1 d f . . 
+        . . f d d d d d d 1 1 1 d f . . 
+        . . f b d d d d b f d 1 d f . . 
+        . . f c b b b d c f d d b f . . 
+        . . . f c b b 1 1 1 1 1 f . . . 
+        . . . . f f f f f 1 b 1 f . . . 
+        . . . . f b 1 1 1 c f b f . . . 
+        . . . . f f b 1 b 1 f f . . . . 
+        . . f . f f f b f b f . . . . . 
+        . . f f f f f f f f . . . . . . 
+        . . . f f f f f . . . . . . . . 
+        `)
+    animation.attachAnimation(mySprite, anim)
+    anim = animation.createAnimation(ActionKind.Invincible, 50)
+    anim.addAnimationFrame(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f 1 1 1 1 f f . . . . 
+        . . . f b 1 1 1 1 1 1 b f . . . 
+        . . . f d 1 1 1 1 1 1 1 f . . . 
+        . . f d d 1 1 1 1 1 1 1 d f . . 
+        . . f d d d 1 1 1 1 1 1 d f . . 
+        . . f d d d d d d 1 1 1 d f . . 
+        . . f b d d d d b f d 1 d f . . 
+        . . f c b b b d c f d d b f . . 
+        . . . f c b b 1 1 1 1 1 f . . . 
+        . . . . f f f f f 1 b 1 f . . . 
+        . . . . f b 1 1 1 c f b f . . . 
+        . . . . f f b 1 b 1 f f . . . . 
+        . . f . f f f b f b f . . . . . 
+        . . f f f f f f f f . . . . . . 
+        . . . f f f f f . . . . . . . . 
+        `)
+    anim.addAnimationFrame(img`
+        . . . . . . f f f a f . . . . . 
+        . . . . f f b 3 3 3 3 f 3 . . . 
+        . . . f b 3 3 3 f f 3 3 b 3 . . 
+        . . . f 3 3 3 f f f f 3 3 f . . 
+        . . f f f f 3 3 f f f f 3 d f . 
+        . f 3 3 3 3 c 3 3 3 3 3 3 d 3 . 
+        . a f 3 3 3 3 f d c f 3 3 b f . 
+        . . f 3 3 f 3 f b 3 3 3 3 3 f . 
+        . . . f f f f a f d b 3 b 3 a . 
+        . . . . 3 c c c f c f b f b f . 
+        . . . . . f f f f f f f a f f . 
+        . . . . . . f 3 a f f 3 . . . . 
+        . . . . . . f a a a a f . . . . 
+        . . a . . 3 f a a a f 3 . . . . 
+        . . f f f f a a a 3 f . . . . . 
+        . . . f 3 f f f f f . . . . . . 
+        `)
+    animation.attachAnimation(mySprite, anim)
+}
 let projectile: Sprite = null
 let gapSprite: Sprite = null
 let gapImage: Image = null
 let bottomImage: Image = null
 let topImage: Image = null
 let gap = 0
+let anim: animation.Animation = null
 let mySprite: Sprite = null
 scene.setBackgroundColor(8)
 info.setScore(0)
 effects.blizzard.startScreenEffect()
-mySprite = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f 1 1 1 1 f f . . . . 
-    . . . f b 1 1 1 1 1 1 b f . . . 
-    . . . f d 1 1 1 1 1 1 1 f . . . 
-    . . f d d 1 1 1 1 1 1 1 d f . . 
-    . . f d d d 1 1 1 1 1 1 d f . . 
-    . . f d d d d d d 1 1 1 d f . . 
-    . . f b d d d d b f d 1 d f . . 
-    . . f c b b b d c f d d b f . . 
-    . . . f c b b 1 1 1 1 1 f . . . 
-    . . . . f f f f f 1 b 1 f . . . 
-    . . . . f b 1 1 1 c f b f . . . 
-    . . . . f f b 1 b 1 f f . . . . 
-    . . f . f f f b f b f . . . . . 
-    . . f f f f f f f f . . . . . . 
-    . . . f f f f f . . . . . . . . 
-    `, SpriteKind.Player)
-mySprite.ay = 300
-animation.runImageAnimation(
-mySprite,
-[img`
-    . . . . . . f f f f f . . . . . 
-    . . . . f f b 1 1 1 1 f f . . . 
-    . . . f b 1 1 1 1 1 1 1 b f . . 
-    . . . f 1 1 1 1 1 1 1 1 1 f . . 
-    . . f f f f 1 1 1 1 1 1 1 d f . 
-    . f b 1 1 1 c 1 d d 1 1 1 d f . 
-    . f f b 1 b 1 f d c f 1 1 b f . 
-    . . f . b f b f b 1 1 1 1 1 f . 
-    . . . f f f f c f d b 1 b 1 f . 
-    . . . . f c c c f c f b f b f . 
-    . . . . . f f f f f f f f f f . 
-    . . . . . . f f f f f f . . . . 
-    . . . . . . f f f f f f . . . . 
-    . . f . . f f f f f f f . . . . 
-    . . f f f f f f f f f . . . . . 
-    . . . f f f f f f f . . . . . . 
-    `,img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f 1 1 1 1 f f . . . . 
-    . . . f b 1 1 1 1 1 1 b f . . . 
-    . . f b d 1 1 1 1 1 1 1 f . . . 
-    . . f d d 1 1 1 1 1 1 1 d f . . 
-    . . f d d d 1 1 1 1 1 1 d f . . 
-    . . f d d d d d d 1 1 1 d f . . 
-    . . f d d d d d d 1 1 1 d f . . 
-    . . f b d d d d d d d 1 d f . . 
-    . . f f b b d d b f d 1 d f . . 
-    . . . f c b b d c f d d b f . . 
-    . . . f f f b d d c c f f f f . 
-    . . . f f f f c f b b b 1 b c f 
-    . . f f f f f f f f c d 1 b 1 f 
-    f f f f f f f f f . . f d f d f 
-    . f f f f f f . . . . . f . f . 
-    `,img`
-    . . . . . . f f f f f . . . . . 
-    . . . . f f 1 1 1 1 1 f . . . . 
-    . . . f b 1 1 1 1 1 1 b f . . . 
-    . . f b d 1 1 1 1 1 1 1 f . . . 
-    . . f d d d 1 1 1 1 1 1 d f . . 
-    . . f d d d d 1 1 1 1 1 d f . . 
-    . . f d d d d d d d 1 1 d f . . 
-    . . f d d d d d d d 1 1 1 f . . 
-    . . f d d d d d d c f 1 1 f . . 
-    . . . f b d d d b 1 1 1 1 b f . 
-    . . . . f f f c f d b 1 b 1 f . 
-    . . . f f f f f f f f b f b f . 
-    f f . f f f f f f f f f f f . . 
-    . f f f f f f f f . . . . . . . 
-    . f f f f f f b 1 b 1 f . . . . 
-    . . f f f f f f b f b f . . . . 
-    `,img`
-    . . . . . . f f f f f . . . . . 
-    . . . . f f b 1 1 1 1 f f . . . 
-    . . . f b 1 1 1 1 1 1 1 b f . . 
-    . . . f 1 1 1 1 1 1 1 1 1 f . . 
-    . . f f f f 1 1 1 1 1 1 1 d f . 
-    . f b 1 1 1 c 1 d d 1 1 1 d f . 
-    . f f b 1 b 1 f d c f 1 1 b f . 
-    . . f . b f b f b 1 1 1 1 1 f . 
-    . . . f f f f c f d b 1 b 1 f . 
-    . . . . f c c c f c f b f b f . 
-    . . . . . f f f f f f f f f f . 
-    . . . . . . f f f f f f . . . . 
-    . . . . . . f f f f f f . . . . 
-    . . f . . f f f f f f f . . . . 
-    . . f f f f f f f f f . . . . . 
-    . . . f f f f f f f . . . . . . 
-    `,img`
-    . . . . . . f f f f f . . . . . 
-    . . . . f f 1 1 1 1 1 f . . . . 
-    . . . f b 1 1 1 1 1 1 b f . . . 
-    . . f b d 1 1 1 1 1 1 1 f . . . 
-    . . f d d d 1 1 1 1 1 1 d f . . 
-    . . f d d d d 1 1 1 1 1 d f . . 
-    . . f d d d d d d d 1 1 d f . . 
-    . . f d d d d d d d 1 1 1 f . . 
-    . . f d d d d d d c f 1 1 f . . 
-    . . . f b d d d b 1 1 1 1 b f . 
-    . . . . f f f c f d b 1 b 1 f . 
-    . . . f f f f f f f f b f b f . 
-    f f . f f f f f f f f f f f . . 
-    . f f f f f f f f . . . . . . . 
-    . f f f f f f b 1 b 1 f . . . . 
-    . . f f f f f f b f b f . . . . 
-    `,img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f 1 1 1 1 f f . . . . 
-    . . . f b 1 1 1 1 1 1 b f . . . 
-    . . f b d 1 1 1 1 1 1 1 f . . . 
-    . . f d d 1 1 1 1 1 1 1 d f . . 
-    . . f d d d 1 1 1 1 1 1 d f . . 
-    . . f d d d d d d 1 1 1 d f . . 
-    . . f d d d d d d 1 1 1 d f . . 
-    . . f b d d d d d d d 1 d f . . 
-    . . f f b b d d b f d 1 d f . . 
-    . . . f c b b d c f d d b f . . 
-    . . . f f f b d d c c f f f f . 
-    . . . f f f f c f b b b 1 b c f 
-    . . f f f f f f f f c d 1 b 1 f 
-    f f f f f f f f f . . f d f d f 
-    . f f f f f f . . . . . f . f . 
-    `],
-200,
-true
-)
+createPlayer()
 info.setLife(3)
 game.onUpdate(function () {
-    if (mySprite.vy > 0) {
+    if (mySprite.vy > 0 && mySprite.kind() == SpriteKind.Player) {
         animation.setAction(mySprite, ActionKind.Idle)
     }
     if (mySprite.bottom > scene.screenHeight() || mySprite.top < 0) {
-        game.over(false)
+        takeDamage()
+        if (mySprite.bottom > scene.screenHeight()) {
+            mySprite.setVelocity(0, -100)
+        } else {
+            mySprite.setVelocity(0, 50)
+        }
     }
 })
 game.onUpdateInterval(1500, function () {
